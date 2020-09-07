35af6c027513e7567dd932d8ea0d1d9c3d53ceb1
Jackelyn Guzman <jackelyn.1768257@nv.ccsd.net>
2020-09-07 05:15:02 +0000

    Completed the code for StudentDesks

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -14,7 +14,20 @@ class StudentDesks {
         /*
          *  your code goes here
          */
-          
+        int desks1 = class1 / 2;
+        int desks2 = class2 / 2;
+        int desks3 = class3 / 2;
+
+        int remainder1 = class1 % 2;
+        int remainder2 = class2 % 2;
+        int remainder3 = class3 % 2;
+
+        int totalRemainders = remainder1 + remainder2 + remainder3; 
+        int totalDesks = desks1 + desks2 + desks3;
+
+        int totalStuDesks = totalRemainders + totalDesks;
+        
+        System.out.print(totalStuDesks);
 
         // closing the scanner object
         scanner.close();
